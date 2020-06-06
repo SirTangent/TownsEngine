a9155ba37168178e2b06ee9ed2970c69c49e8eed
Omar Radwan <radwano@wit.edu>
2020-06-06 12:12:27 -0400

    Added: comments and classes

--- src/main/java/ToolBelt.java
+++ src/main/java/ToolBelt.java
@@ -1,9 +1,24 @@
+import org.jetbrains.annotations.NotNull;
+
+/**
+This class is a general class with useful function that can be used in more then one place.
+
+@author Omar Radwan
+@version 1.0.0
+ */
 public class ToolBelt {
+    /**
+     * This method clears the console by creating a lot of empty lines.
+     */
     public static void clearScreen() {
         for (int i = 0; i < 80; i++)
             System.out.println();
     }
 
+    /**
+     * This method will make the program stop of a set number of seconds
+     * @param sec The number of second that you would like the program to stop for
+     */
     public static void sleep(int sec) {
         try {
             Thread.sleep(sec * 1000);
@@ -12,7 +27,12 @@ public class ToolBelt {
         }
     }
 
-    public static void slowText(String text, Boolean newLine) {
+    /**
+     * This method will print out the line char by char at a slow speed.
+     * @param text The String you want to print out this way
+     * @param newLine If it is set to true, after the text is done printing it will print a new line.
+     */
+    public static void slowText(@NotNull String text, Boolean newLine) {
         for (int i = 0; i < text.length(); i++) {
             System.out.print(text.charAt(i));
             try {
@@ -27,20 +47,20 @@ public class ToolBelt {
         }
     }
 
+    /**
+     * This is will call slowText(String text, Boolean newLine). This acts as a default value input for newLine.
+     * @param text see slowText(String text, Boolean newLine).
+     */
     public static void slowText(String text) {
         slowText(text, true);
     }
 
-    public static void slowTextLine(String text) {
-        System.out.println(text);
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void fastText(String text, Boolean newLine) {
+    /**
+     * This method will print out the line char by char at a fast speed.
+     * @param textThe String you want to print out this way
+     * @param newLine If it is set to true, after the text is done printing it will print a new line.
+     */
+    public static void fastText(@NotNull String text, Boolean newLine) {
         for (int i = 0; i < text.length(); i++) {
             System.out.print(text.charAt(i));
             try {
@@ -55,10 +75,20 @@ public class ToolBelt {
         }
     }
 
+    /**
+     * This is will call fastText(String text, Boolean newLine). This acts as a default value input for newLine.
+     * @param text see fastText(String text, Boolean newLine).
+     */
     public static void fastText(String text) {
         fastText(text, true);
     }
 
+    /**
+     * This will pick a random number between the min and max values
+     * @param min The number that is the min
+     * @param max The number that is the max
+     * @return Returns a random int between the two ranges
+     */
     public static int random(int min, int max) {
         int range = (max - min) + 1;
         return (int) (Math.random() * range) + min;
